b55a9feb16ed08708b61078b299a509d52490187
karan <karanvaswani2002@gmail.com>
2023-02-07 22:23:42 +0530

    updated features

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import possibilityImage from '../../assets/nxtgen.png'
 import './possibility.css'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='nextgen__possibility section__padding' id='possibility'>
+      <div className='nextgen__possibility-image'>
+      <img src={possibilityImage} alt='possibility'/>
+      </div>
+    </div>
   )
 }
 
