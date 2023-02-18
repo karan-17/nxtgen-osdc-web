9aecf9ba28680322eb01a17dffb4bb3b46113de7
karan <karanvaswani2002@gmail.com>
2023-02-19 04:10:14 +0530

    possibilities section completed

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,6 +8,18 @@ const Possibility = () => {
       <div className='nextgen__possibility-image'>
       <img src={possibilityImage} alt='possibility'/>
       </div>
+      <div className='nextgen__possibility-content'>
+      <h4>Request Early Access to Get Started</h4>
+      <h1 className='gradient__text'>The Possibilities are beyond your imaginations</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed euismod, nisl sed lacinia tincidunt, nunc elit
+        tincidunt massa, eget aliquam nunc justo vel mauris.
+        Sed euismod, nisl sed lacinia tincidunt, nunc elit
+        tincidunt massa, eget aliquam nunc justo vel mauris.
+      </p>
+      <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
 }
