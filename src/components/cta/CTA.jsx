145e7f6cb0469caed18ac90644594ff6cd1b85a7
karan <karanvaswani2002@gmail.com>
2023-02-19 18:44:46 +0530

    CTA section completed

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,24 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='nextgen__cta'>
+    <div className='nextgen__cta-content'>
+    <p>
+      <span className='nextgen__cta-content--bold'>NextGen</span> is a new
+      generation of <span className='nextgen__cta-content--bold'>digital</span>{' '}
+      and <span className='nextgen__cta-content--bold'>social</span> media
+      marketing agency. We are a team of <span className='nextgen__cta-content--bold'>creative</span>{' '}
+      and <span className='nextgen__cta-content--bold'>passionate</span> people
+      who are dedicated to helping your business grow.
+    </p>
+    <h3>
+      Register today for early access.
+    </h3>
+    </div>
+    <div className='nextgen__cta-btn'>
+    <button type='button'>Register</button>
+    </div>
+    </div>
   )
 }
 
